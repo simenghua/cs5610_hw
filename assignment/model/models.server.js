c8167046b078284a271ce414dc1217303d45faaf
Simeng <simeng.hua@gmail.com>
2018-03-30 02:46:35 -0700

    fix mongodb

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/cs5610', {useMongoClient: true});
-
-module.exports = db;
+// var mongoose = require('mongoose');
+// var db = mongoose.connect('mongodb://localhost:27017/cs5610', {useMongoClient: true});
+//
+// module.exports = db;
