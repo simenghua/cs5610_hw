bdf74ad96a27d25553bd257b72061481b8408bfa
Simeng <simeng.hua@gmail.com>
2018-04-11 23:26:39 -0700

    update

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -77,7 +77,7 @@ module.exports = function (app) {
                 token: token
               }
             };
-            return model.userModel.createUser(newFacebookUser);
+            return userModel.createUser(newFacebookUser);
           }
         },
         function(err) {
